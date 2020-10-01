572a7a72043443667956c9d220f46eefe2007e9e
vitoriahelen-git <vitoriahellen3004@gmail.com>
2020-10-01 16:46:14 -0300

    Leia 2 numeros inteiros e exiba o resultado da soma:

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Soma2numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int digite; 
+            int digite2; 
+            int Soma;
+
+            Console.Write("Digite o primeiro número:");
+            digite = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o sengundo número:");
+            digite2 = Convert.ToInt32(Console.ReadLine());
+            Soma = digite + digite2; 
+            Console.WriteLine($"{digite} + {digite2} = {Soma}"); 
+
+            
+    
+             
+
+        }
+    }
+}
